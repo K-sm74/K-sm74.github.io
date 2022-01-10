479e71d925707858c54ca3039931dda79e629163
Kz-sm74 <smkaigan1211@gmail.com>
2022-01-10 12:41:28 +0900

    update

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 let carender = new Date();
 
+let weekdays = ["Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"];
+
 let date = carender.toLocaleDateString();
-let info = "Today is " + date;
+let info = date;
+let= week = weekdays[carender.getDay()];
 
 document.getElementById("inf").textContent = info;
-
+document.getElementById("week").textContent = week;
 
 console.log(carender.toLocaleString());
 console.log(carender.toLocaleDateString());
